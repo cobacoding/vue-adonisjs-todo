dd4b43fa72bce4feea5d84782839934ec20551cb
suka_coding <codingsuka@gmail.com>
2021-05-10 14:28:45 +0700

    initial route

--- server/start/routes.js
+++ server/start/routes.js
@@ -19,3 +19,9 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+Route.post('auth/register', () => {
+  return {
+    message: 'hello post',
+  }
+})
